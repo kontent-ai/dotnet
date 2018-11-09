vit-svoboda/delivery-sdk-net

DRAFT-4768 Add tests for different DI frameworks

KenticoCloud.Delivery.Tests/DIFrameworks/Helpers/FakeModelProvider.cs
@@ -1,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KenticoCloud.Delivery.Tests.DIFrameworks.Helpers
+{
+    internal class FakeModelProvider : ICodeFirstModelProvider
+    {
+        public T GetContentItemModel<T>(JToken item, JToken modularContent) 
+            => throw new NotImplementedException();
+    }
+}

kontent-ai/dotnet

src/delivery/KenticoCloud.Delivery.Tests/DependencyInjectionFrameworks/Helpers/FakeModelProvider.cs
@@ -1,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KenticoCloud.Delivery.Tests.DependencyInjectionFrameworks.Helpers
+{
+    internal class FakeModelProvider : ICodeFirstModelProvider
+    {
+        public T GetContentItemModel<T>(JToken item, JToken modularContent) 
+            => throw new NotImplementedException();
+    }
+}