mrnustik/management-sdk-net

EN-261 Add schedule publish and unpublish endpoint to Management API SDK

Kontent.Ai.Management.Tests/ManagementClientTests/PublishingTests.cs
@@ -131,6 +131,46 @@ public class PublishingTests
         await client.Invoking(x => x.SchedulePublishingOfLanguageVariantAsync(null, schedule)).Should().ThrowAsync<ArgumentNullException>();
     }
 
+    [Theory]
+    [ClassData(typeof(CombinationOfVariantIdentifiersAndUrl))]
+    public async void SchedulePublishAndUnpublishOfLanguageVariantAsync_SchedulesVariant(LanguageVariantIdentifier variantIdentifier, string expectedUrl)
+    {
+        var client = _scenario.CreateManagementClient();
+
+        var schedule = new SchedulePublishAndUnpublishModel()
+        {
+            PublishDisplayTimeZone = "prague",
+            PublishScheduledTo = DateTimeOffset.UtcNow,
+            UnpublishDisplayTimeZone = "prague",
+            UnpublishScheduledTo = DateTimeOffset.UtcNow.AddDays(10)
+        };
+
+        await client.SchedulePublishAndUnpublishOfLanguageVariantAsync(variantIdentifier, schedule);
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Put)
+            .RequestPayload(schedule)
+            .Url($"{expectedUrl}/schedule-publish-and-unpublish")
+            .Validate();
+    }
+
+    [Fact]
+    public async void SchedulePublishAndUnpublishOfLanguageVariantAsync_NoIdentifier_Throws()
+    {
+        var client = _scenario.CreateManagementClient();
+
+        var schedule = new SchedulePublishAndUnpublishModel()
+        {
+            PublishDisplayTimeZone = "prague",
+            PublishScheduledTo = DateTimeOffset.UtcNow,
+            UnpublishDisplayTimeZone = "prague",
+            UnpublishScheduledTo = DateTimeOffset.UtcNow.AddDays(10)
+        };
+
+        await client.Invoking(x => x.SchedulePublishAndUnpublishOfLanguageVariantAsync(null, schedule)).Should().ThrowAsync<ArgumentNullException>();
+    }
+    
     [Fact]
     public async void SchedulePublishingOfLanguageVariantAsync_ScheduleModelIsNull_Throws()
     {

Kontent.Ai.Management/IManagementClient.cs
@@ -606,6 +606,13 @@ public interface IManagementClient
     Task ScheduleUnpublishingOfLanguageVariantAsync(LanguageVariantIdentifier identifier, ScheduleModel scheduleModel);
 
     /// <summary>
+    /// Schedules publish and unpublish of language variant.
+    /// </summary>
+    /// <param name="identifier">The identifier of the language variant that should be scheduled.</param>
+    /// <param name="schedule">The interval in which the variant should be published</param>
+    Task SchedulePublishAndUnpublishOfLanguageVariantAsync(LanguageVariantIdentifier identifier, SchedulePublishAndUnpublishModel schedule);
+
+    /// <summary>
     /// Unpublishes the language variant.
     /// </summary>
     /// <param name="identifier">Identifier of the language variant to be unpublished.</param>

Kontent.Ai.Management/ManagementClient.Publishing.cs
@@ -45,6 +45,18 @@ public sealed partial class ManagementClient
 
         await _actionInvoker.InvokeMethodAsync(endpointUrl, HttpMethod.Put, scheduleModel);
     }
+    
+    /// <inheritdoc />
+    public async Task SchedulePublishAndUnpublishOfLanguageVariantAsync(LanguageVariantIdentifier identifier, SchedulePublishAndUnpublishModel schedule) 
+    {
+        ArgumentNullException.ThrowIfNull(identifier);
+        
+        ArgumentNullException.ThrowIfNull(schedule);
+
+        var endpointUrl = _urlBuilder.BuildSchedulePublishAndUnpublishVariantUrl(identifier);
+
+        await _actionInvoker.InvokeMethodAsync(endpointUrl, HttpMethod.Put, schedule);
+    }
 
     /// <inheritdoc />
     public async Task CancelPublishingOfLanguageVariantAsync(LanguageVariantIdentifier identifier)

kontent-ai/dotnet

src/management/Kontent.Ai.Management.Tests/ManagementClientTests/PublishingTests.cs
@@ -130,6 +130,46 @@ public class PublishingTests
         await client.Invoking(x => x.SchedulePublishingOfLanguageVariantAsync(null, schedule)).Should().ThrowAsync<ArgumentNullException>();
     }
 
+    [Theory]
+    [ClassData(typeof(CombinationOfVariantIdentifiersAndUrl))]
+    public async void SchedulePublishingAndUnpublishingOfLanguageVariantAsync_SchedulesVariant(LanguageVariantIdentifier variantIdentifier, string expectedUrl)
+    {
+        var client = _scenario.CreateManagementClient();
+
+        var schedule = new SchedulePublishAndUnpublishModel()
+        {
+            PublishDisplayTimeZone = "prague",
+            PublishScheduledTo = DateTimeOffset.UtcNow,
+            UnpublishDisplayTimeZone = "prague",
+            UnpublishScheduledTo = DateTimeOffset.UtcNow.AddDays(10)
+        };
+
+        await client.SchedulePublishingAndUnpublishingOfLanguageVariantAsync(variantIdentifier, schedule);
+
+        _scenario
+            .CreateExpectations()
+            .HttpMethod(HttpMethod.Put)
+            .RequestPayload(schedule)
+            .Url($"{expectedUrl}/schedule-publish-and-unpublish")
+            .Validate();
+    }
+
+    [Fact]
+    public async void SchedulePublishingAndUnpublishingOfLanguageVariantAsync_NoIdentifier_Throws()
+    {
+        var client = _scenario.CreateManagementClient();
+
+        var schedule = new SchedulePublishAndUnpublishModel()
+        {
+            PublishDisplayTimeZone = "prague",
+            PublishScheduledTo = DateTimeOffset.UtcNow,
+            UnpublishDisplayTimeZone = "prague",
+            UnpublishScheduledTo = DateTimeOffset.UtcNow.AddDays(10)
+        };
+
+        await client.Invoking(x => x.SchedulePublishingAndUnpublishingOfLanguageVariantAsync(null, schedule)).Should().ThrowAsync<ArgumentNullException>();
+    }
+    
     [Fact]
     public async void SchedulePublishingOfLanguageVariantAsync_ScheduleModelIsNull_Throws()
     {

src/management/Kontent.Ai.Management/IManagementClient.cs
@@ -608,6 +608,13 @@ public interface IManagementClient
     Task ScheduleUnpublishingOfLanguageVariantAsync(LanguageVariantIdentifier identifier, ScheduleModel scheduleModel);
 
     /// <summary>
+    /// Schedules publish and unpublish of language variant.
+    /// </summary>
+    /// <param name="identifier">The identifier of the language variant that should be scheduled.</param>
+    /// <param name="schedule">The interval in which the variant should be published</param>
+    Task SchedulePublishingAndUnpublishingOfLanguageVariantAsync(LanguageVariantIdentifier identifier, SchedulePublishAndUnpublishModel schedule);
+
+    /// <summary>
     /// Unpublishes the language variant.
     /// </summary>
     /// <param name="identifier">Identifier of the language variant to be unpublished.</param>

src/management/Kontent.Ai.Management/ManagementClient.Publishing.cs
@@ -44,6 +44,18 @@ public sealed partial class ManagementClient
 
         await _actionInvoker.InvokeMethodAsync(endpointUrl, HttpMethod.Put, scheduleModel);
     }
+    
+    /// <inheritdoc />
+    public async Task SchedulePublishingAndUnpublishingOfLanguageVariantAsync(LanguageVariantIdentifier identifier, SchedulePublishAndUnpublishModel schedule) 
+    {
+        ArgumentNullException.ThrowIfNull(identifier);
+        
+        ArgumentNullException.ThrowIfNull(schedule);
+
+        var endpointUrl = _urlBuilder.BuildSchedulePublishAndUnpublishVariantUrl(identifier);
+
+        await _actionInvoker.InvokeMethodAsync(endpointUrl, HttpMethod.Put, schedule);
+    }
 
     /// <inheritdoc />
     public async Task CancelPublishingOfLanguageVariantAsync(LanguageVariantIdentifier identifier)