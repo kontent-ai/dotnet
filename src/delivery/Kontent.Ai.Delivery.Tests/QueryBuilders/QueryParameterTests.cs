kontent-ai/delivery-sdk-net

pre-production cleanup, harden null handling, rich text, fix typos

Kontent.Ai.Delivery.Tests/QueryBuilders/QueryParameterTests.cs
@@ -1,4 +1,5 @@
 using Kontent.Ai.Delivery.Abstractions;
+using Kontent.Ai.Delivery.Tests.Models.ContentTypes;
 using Microsoft.Extensions.DependencyInjection;
 using RichardSzalay.MockHttp;
 using Xunit;
@@ -14,9 +15,17 @@ public sealed class QueryParameterTests
     private readonly string _env = Guid.NewGuid().ToString();
     private string BaseUrl => $"https://deliver.kontent.ai/{_env}";
 
-    private IDeliveryClient CreateClient(MockHttpMessageHandler mockHttp, DeliveryOptions? options = null)
+    private IDeliveryClient CreateClient(
+        MockHttpMessageHandler mockHttp,
+        DeliveryOptions? options = null,
+        ITypeProvider? typeProvider = null)
     {
         var services = new ServiceCollection();
+        if (typeProvider is not null)
+        {
+            services.AddSingleton(typeProvider);
+        }
+
         var opts = options ?? new DeliveryOptions { EnvironmentId = _env };
         services.AddDeliveryClient(opts, configureHttpClient: b => b.ConfigurePrimaryHttpMessageHandler(() => mockHttp));
         return services.BuildServiceProvider().GetRequiredService<IDeliveryClient>();
@@ -252,6 +261,56 @@ public sealed class QueryParameterTests
         Assert.True(result.IsSuccess, $"Request failed: {result.Error?.Message}");
     }
 
+    [Fact]
+    public async Task GetItem_WaitForLoadingNewContentFalse_OverridesGlobalWaitAndOmitsHeader()
+    {
+        // Arrange - global wait enabled, query explicitly disables it
+        var mock = new MockHttpMessageHandler();
+        var itemJson = BuildMinimalItemJson("test_item");
+
+        mock.When($"{BaseUrl}/items/test_item")
+            .With(req => !req.Headers.Contains("X-KC-Wait-For-Loading-New-Content"))
+            .Respond("application/json", itemJson);
+
+        var client = CreateClient(mock, new DeliveryOptions
+        {
+            EnvironmentId = _env,
+            WaitForLoadingNewContent = true
+        });
+
+        // Act
+        var result = await client.GetItem<IDynamicElements>("test_item")
+            .WaitForLoadingNewContent(false)
+            .ExecuteAsync();
+
+        // Assert
+        Assert.True(result.IsSuccess, $"Request failed: {result.Error?.Message}");
+    }
+
+    [Fact]
+    public async Task GetItems_GenericExecuteTwice_DoesNotDuplicateAutoTypeFilter()
+    {
+        // Arrange - repeated execution on the same query should keep a stable system.type filter
+        var mock = new MockHttpMessageHandler();
+        var itemsJson = BuildMinimalItemsListingJson(["item_1"]);
+        const string typeFilterKey = "system.type%5Beq%5D";
+
+        mock.When($"{BaseUrl}/items")
+            .With(req => CountOccurrences(req.RequestUri!.Query, typeFilterKey) == 1)
+            .Respond("application/json", itemsJson);
+
+        var client = CreateClient(mock, typeProvider: new StaticTypeProvider());
+        var query = client.GetItems<Article>();
+
+        // Act
+        var firstResult = await query.ExecuteAsync();
+        var secondResult = await query.ExecuteAsync();
+
+        // Assert
+        Assert.True(firstResult.IsSuccess, $"Request failed: {firstResult.Error?.Message}");
+        Assert.True(secondResult.IsSuccess, $"Request failed: {secondResult.Error?.Message}");
+    }
+
     #endregion
 
     #region Combined Parameters Tests
@@ -367,6 +426,35 @@ public sealed class QueryParameterTests
                 "modular_content": {}
             }
             """;
+    }
+
+    private static int CountOccurrences(string input, string value)
+    {
+        if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(value))
+        {
+            return 0;
+        }
+
+        var count = 0;
+        var index = 0;
+        while ((index = input.IndexOf(value, index, StringComparison.Ordinal)) >= 0)
+        {
+            count++;
+            index += value.Length;
+        }
+
+        return count;
+    }
+
+    private sealed class StaticTypeProvider : ITypeProvider
+    {
+        public Type? GetType(string contentType)
+            => string.Equals(contentType, "article", StringComparison.OrdinalIgnoreCase)
+                ? typeof(Article)
+                : null;
+
+        public string? GetCodename(Type contentType)
+            => contentType == typeof(Article) ? "article" : null;
     }
 
     #endregion

Kontent.Ai.Delivery.Tests/QueryBuilders/UsedInQueriesTests.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Text;
+using Kontent.Ai.Delivery;
 using Kontent.Ai.Delivery.Abstractions;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using RichardSzalay.MockHttp;
 using Xunit;
@@ -23,7 +25,11 @@ public sealed class UsedInQueriesTests
             .WithHeaders("X-Continuation", "token_1")
             .Respond(_ => CreateUsedInResponse(["parent_3"], continuationToken: null));
 
-        var client = BuildClient(env, mockHttp);
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            EnableResilience = false
+        });
         var items = new List<IUsedInItem>();
 
         await foreach (var item in client.GetItemUsedIn("coffee_beverages_explained").EnumerateItemsAsync())
@@ -44,10 +50,13 @@ public sealed class UsedInQueriesTests
             .Respond(_ => CreateUsedInResponse(["parent_1"], continuationToken: "token_1"));
 
         mockHttp.Expect(usedInUrl)
-            .WithHeaders("X-Continuation", "token_1")
             .Respond(HttpStatusCode.ServiceUnavailable);
 
-        var client = BuildClient(env, mockHttp);
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            EnableResilience = false
+        });
         var items = new List<IUsedInItem>();
 
         await foreach (var item in client.GetAssetUsedIn("asset_codename").EnumerateItemsAsync())
@@ -58,11 +67,121 @@ public sealed class UsedInQueriesTests
         mockHttp.VerifyNoOutstandingExpectation();
     }
 
-    private static IDeliveryClient BuildClient(string env, MockHttpMessageHandler mockHttp)
+    [Fact]
+    public async Task UsedInQueries_FailedIntermediatePage_WithStatusEnumeration_YieldsFailureResult()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/assets/asset_codename/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+        var callCount = 0;
+        mockHttp.When(usedInUrl)
+            .Respond(_ =>
+            {
+                callCount++;
+                return callCount == 1
+                    ? CreateUsedInResponse(["parent_1"], continuationToken: "token_1")
+                    : new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            });
+
+        var client = BuildClient(env, mockHttp);
+        var pageResults = new List<IDeliveryResult<IReadOnlyList<IUsedInItem>>>();
+
+        await foreach (var page in client.GetAssetUsedIn("asset_codename").EnumerateItemsWithStatusAsync())
+        {
+            pageResults.Add(page);
+        }
+
+        Assert.Equal(2, pageResults.Count);
+        Assert.True(pageResults[0].IsSuccess);
+        Assert.Single(pageResults[0].Value);
+        Assert.Equal("parent_1", pageResults[0].Value[0].System.Codename);
+        Assert.False(pageResults[1].IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsedInQueries_GlobalWaitEnabled_QueryWaitFalse_OmitsHeader()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/items/coffee_beverages_explained/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+
+        mockHttp.Expect(usedInUrl)
+            .With(req => !req.Headers.Contains("X-KC-Wait-For-Loading-New-Content"))
+            .Respond(_ => CreateUsedInResponse(["parent_1"], continuationToken: null));
+
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            WaitForLoadingNewContent = true
+        });
+
+        var items = new List<IUsedInItem>();
+        await foreach (var item in client.GetItemUsedIn("coffee_beverages_explained")
+                           .WaitForLoadingNewContent(false)
+                           .EnumerateItemsAsync())
+        {
+            items.Add(item);
+        }
+
+        Assert.Single(items);
+        Assert.Equal("parent_1", items[0].System.Codename);
+        mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Fact]
+    public async Task UsedInQueries_FailedIntermediatePage_EmitsPaginationStoppedEarlyWarning()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/assets/asset_codename/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+        var callCount = 0;
+        mockHttp.When(usedInUrl)
+            .Respond(_ =>
+            {
+                callCount++;
+                return callCount == 1
+                    ? CreateUsedInResponse(["parent_1"], continuationToken: "token_1")
+                    : new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            });
+
+        var loggerProvider = new CollectingLoggerProvider();
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            EnableResilience = false
+        }, loggerProvider);
+
+        await foreach (var _ in client.GetAssetUsedIn("asset_codename").EnumerateItemsAsync())
+        {
+            // Intentionally empty - verify log side effect.
+        }
+
+        Assert.Contains(
+            loggerProvider.Entries,
+            entry => entry.EventId == Kontent.Ai.Delivery.Logging.LogEventIds.PaginationStoppedEarly &&
+                     entry.Message.Contains("AssetUsedIn", StringComparison.Ordinal));
+    }
+
+    private static IDeliveryClient BuildClient(
+        string env,
+        MockHttpMessageHandler mockHttp,
+        DeliveryOptions? options = null,
+        ILoggerProvider? loggerProvider = null)
     {
         var services = new ServiceCollection();
+        if (loggerProvider is not null)
+        {
+            services.AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Debug);
+                builder.AddProvider(loggerProvider);
+            });
+        }
+
+        options ??= new DeliveryOptions { EnvironmentId = env };
         services.AddDeliveryClient(
-            new DeliveryOptions { EnvironmentId = env },
+            options,
             configureHttpClient: b => b.ConfigurePrimaryHttpMessageHandler(() => mockHttp));
 
         return services.BuildServiceProvider().GetRequiredService<IDeliveryClient>();
@@ -82,5 +201,46 @@ public sealed class UsedInQueriesTests
             response.Headers.Add("X-Continuation", continuationToken);
 
         return response;
+    }
+
+    private sealed record LogEntry(int EventId, string Message);
+
+    private sealed class CollectingLoggerProvider : ILoggerProvider
+    {
+        private readonly List<LogEntry> _entries = [];
+
+        public IReadOnlyList<LogEntry> Entries => _entries;
+
+        public ILogger CreateLogger(string categoryName) => new CollectingLogger(_entries);
+
+        public void Dispose()
+        {
+        }
+    }
+
+    private sealed class CollectingLogger(List<LogEntry> entries) : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state) where TState : notnull => NoopDisposable.Instance;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception? exception,
+            Func<TState, Exception?, string> formatter)
+        {
+            entries.Add(new LogEntry(eventId.Id, formatter(state, exception)));
+        }
+    }
+
+    private sealed class NoopDisposable : IDisposable
+    {
+        public static NoopDisposable Instance { get; } = new();
+
+        public void Dispose()
+        {
+        }
     }
 }

kontent-ai/dotnet

src/delivery/Kontent.Ai.Delivery.Tests/QueryBuilders/QueryParameterTests.cs
@@ -1,4 +1,5 @@
 using Kontent.Ai.Delivery.Abstractions;
+using Kontent.Ai.Delivery.Tests.Models.ContentTypes;
 using Microsoft.Extensions.DependencyInjection;
 using RichardSzalay.MockHttp;
 using Xunit;
@@ -14,9 +15,17 @@ public sealed class QueryParameterTests
     private readonly string _env = Guid.NewGuid().ToString();
     private string BaseUrl => $"https://deliver.kontent.ai/{_env}";
 
-    private IDeliveryClient CreateClient(MockHttpMessageHandler mockHttp, DeliveryOptions? options = null)
+    private IDeliveryClient CreateClient(
+        MockHttpMessageHandler mockHttp,
+        DeliveryOptions? options = null,
+        ITypeProvider? typeProvider = null)
     {
         var services = new ServiceCollection();
+        if (typeProvider is not null)
+        {
+            services.AddSingleton(typeProvider);
+        }
+
         var opts = options ?? new DeliveryOptions { EnvironmentId = _env };
         services.AddDeliveryClient(opts, configureHttpClient: b => b.ConfigurePrimaryHttpMessageHandler(() => mockHttp));
         return services.BuildServiceProvider().GetRequiredService<IDeliveryClient>();
@@ -209,30 +218,6 @@ public sealed class QueryParameterTests
     }
 
     [Fact]
-    public async Task GetItem_WaitForLoadingNewContentFromOptions_AddsHeader()
-    {
-        // Arrange - WaitForLoadingNewContent set via DeliveryOptions
-        var mock = new MockHttpMessageHandler();
-        var itemJson = BuildMinimalItemJson("test_item");
-
-        mock.When($"{BaseUrl}/items/test_item")
-            .With(req => req.Headers.Contains("X-KC-Wait-For-Loading-New-Content"))
-            .Respond("application/json", itemJson);
-
-        var client = CreateClient(mock, new DeliveryOptions
-        {
-            EnvironmentId = _env,
-            WaitForLoadingNewContent = true
-        });
-
-        // Act
-        var result = await client.GetItem<IDynamicElements>("test_item").ExecuteAsync();
-
-        // Assert
-        Assert.True(result.IsSuccess, $"Request failed: {result.Error?.Message}");
-    }
-
-    [Fact]
     public async Task GetItem_WithoutWaitForLoadingNewContent_DoesNotAddHeader()
     {
         // Arrange - Default behavior without the header
@@ -250,6 +235,52 @@ public sealed class QueryParameterTests
 
         // Assert
         Assert.True(result.IsSuccess, $"Request failed: {result.Error?.Message}");
+    }
+
+    [Fact]
+    public async Task GetItem_WaitForLoadingNewContentFalse_OmitsHeader()
+    {
+        // Arrange
+        var mock = new MockHttpMessageHandler();
+        var itemJson = BuildMinimalItemJson("test_item");
+
+        mock.When($"{BaseUrl}/items/test_item")
+            .With(req => !req.Headers.Contains("X-KC-Wait-For-Loading-New-Content"))
+            .Respond("application/json", itemJson);
+
+        var client = CreateClient(mock);
+
+        // Act
+        var result = await client.GetItem<IDynamicElements>("test_item")
+            .WaitForLoadingNewContent(false)
+            .ExecuteAsync();
+
+        // Assert
+        Assert.True(result.IsSuccess, $"Request failed: {result.Error?.Message}");
+    }
+
+    [Fact]
+    public async Task GetItems_GenericExecuteTwice_DoesNotDuplicateAutoTypeFilter()
+    {
+        // Arrange - repeated execution on the same query should keep a stable system.type filter
+        var mock = new MockHttpMessageHandler();
+        var itemsJson = BuildMinimalItemsListingJson(["item_1"]);
+        const string typeFilterKey = "system.type%5Beq%5D";
+
+        mock.When($"{BaseUrl}/items")
+            .With(req => CountOccurrences(req.RequestUri!.Query, typeFilterKey) == 1)
+            .Respond("application/json", itemsJson);
+
+        var client = CreateClient(mock, typeProvider: new StaticTypeProvider());
+        var query = client.GetItems<Article>();
+
+        // Act
+        var firstResult = await query.ExecuteAsync();
+        var secondResult = await query.ExecuteAsync();
+
+        // Assert
+        Assert.True(firstResult.IsSuccess, $"Request failed: {firstResult.Error?.Message}");
+        Assert.True(secondResult.IsSuccess, $"Request failed: {secondResult.Error?.Message}");
     }
 
     #endregion
@@ -367,6 +398,35 @@ public sealed class QueryParameterTests
                 "modular_content": {}
             }
             """;
+    }
+
+    private static int CountOccurrences(string input, string value)
+    {
+        if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(value))
+        {
+            return 0;
+        }
+
+        var count = 0;
+        var index = 0;
+        while ((index = input.IndexOf(value, index, StringComparison.Ordinal)) >= 0)
+        {
+            count++;
+            index += value.Length;
+        }
+
+        return count;
+    }
+
+    private sealed class StaticTypeProvider : ITypeProvider
+    {
+        public Type? GetType(string contentType)
+            => string.Equals(contentType, "article", StringComparison.OrdinalIgnoreCase)
+                ? typeof(Article)
+                : null;
+
+        public string? GetCodename(Type contentType)
+            => contentType == typeof(Article) ? "article" : null;
     }
 
     #endregion

src/delivery/Kontent.Ai.Delivery.Tests/QueryBuilders/UsedInQueriesTests.cs
@@ -1,7 +1,9 @@
 using System.Net;
 using System.Text;
+using Kontent.Ai.Delivery;
 using Kontent.Ai.Delivery.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using RichardSzalay.MockHttp;
 using Xunit;
 
@@ -23,7 +25,11 @@ public sealed class UsedInQueriesTests
             .WithHeaders("X-Continuation", "token_1")
             .Respond(_ => CreateUsedInResponse(["parent_3"], continuationToken: null));
 
-        var client = BuildClient(env, mockHttp);
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            EnableResilience = false
+        });
         var items = new List<IUsedInItem>();
 
         await foreach (var item in client.GetItemUsedIn("coffee_beverages_explained").EnumerateItemsAsync())
@@ -44,10 +50,13 @@ public sealed class UsedInQueriesTests
             .Respond(_ => CreateUsedInResponse(["parent_1"], continuationToken: "token_1"));
 
         mockHttp.Expect(usedInUrl)
-            .WithHeaders("X-Continuation", "token_1")
             .Respond(HttpStatusCode.ServiceUnavailable);
 
-        var client = BuildClient(env, mockHttp);
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            EnableResilience = false
+        });
         var items = new List<IUsedInItem>();
 
         await foreach (var item in client.GetAssetUsedIn("asset_codename").EnumerateItemsAsync())
@@ -58,11 +67,143 @@ public sealed class UsedInQueriesTests
         mockHttp.VerifyNoOutstandingExpectation();
     }
 
-    private static IDeliveryClient BuildClient(string env, MockHttpMessageHandler mockHttp)
+    [Fact]
+    public async Task UsedInQueries_FailedIntermediatePage_WithStatusEnumeration_YieldsFailureResult()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/assets/asset_codename/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+        var callCount = 0;
+        mockHttp.When(usedInUrl)
+            .Respond(_ =>
+            {
+                callCount++;
+                return callCount == 1
+                    ? CreateUsedInResponse(["parent_1"], continuationToken: "token_1")
+                    : new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            });
+
+        var client = BuildClient(env, mockHttp);
+        var pageResults = new List<IDeliveryResult<IReadOnlyList<IUsedInItem>>>();
+
+        await foreach (var page in client.GetAssetUsedIn("asset_codename").EnumerateItemsWithStatusAsync())
+        {
+            pageResults.Add(page);
+        }
+
+        Assert.Equal(2, pageResults.Count);
+        Assert.True(pageResults[0].IsSuccess);
+        Assert.Single(pageResults[0].Value);
+        Assert.Equal("parent_1", pageResults[0].Value[0].System.Codename);
+        Assert.False(pageResults[1].IsSuccess);
+    }
+
+    [Fact]
+    public async Task UsedInQueries_QueryWaitEnabled_AddsHeader()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/items/coffee_beverages_explained/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+
+        mockHttp.Expect(usedInUrl)
+            .With(req => req.Headers.Contains("X-KC-Wait-For-Loading-New-Content"))
+            .Respond(_ => CreateUsedInResponse(["parent_1"], continuationToken: null));
+
+        var client = BuildClient(env, mockHttp, new DeliveryOptions { EnvironmentId = env });
+        var items = new List<IUsedInItem>();
+
+        await foreach (var item in client.GetItemUsedIn("coffee_beverages_explained")
+                           .WaitForLoadingNewContent()
+                           .EnumerateItemsAsync())
+        {
+            items.Add(item);
+        }
+
+        Assert.Single(items);
+        Assert.Equal("parent_1", items[0].System.Codename);
+        mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Fact]
+    public async Task UsedInQueries_QueryWaitFalse_OmitsHeader()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/items/coffee_beverages_explained/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+
+        mockHttp.Expect(usedInUrl)
+            .With(req => !req.Headers.Contains("X-KC-Wait-For-Loading-New-Content"))
+            .Respond(_ => CreateUsedInResponse(["parent_1"], continuationToken: null));
+
+        var client = BuildClient(env, mockHttp, new DeliveryOptions { EnvironmentId = env });
+
+        var items = new List<IUsedInItem>();
+        await foreach (var item in client.GetItemUsedIn("coffee_beverages_explained")
+                           .WaitForLoadingNewContent(false)
+                           .EnumerateItemsAsync())
+        {
+            items.Add(item);
+        }
+
+        Assert.Single(items);
+        Assert.Equal("parent_1", items[0].System.Codename);
+        mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Fact]
+    public async Task UsedInQueries_FailedIntermediatePage_EmitsPaginationStoppedEarlyWarning()
+    {
+        var env = Guid.NewGuid().ToString();
+        var usedInUrl = $"https://deliver.kontent.ai/{env}/assets/asset_codename/used-in";
+        var mockHttp = new MockHttpMessageHandler();
+        var callCount = 0;
+        mockHttp.When(usedInUrl)
+            .Respond(_ =>
+            {
+                callCount++;
+                return callCount == 1
+                    ? CreateUsedInResponse(["parent_1"], continuationToken: "token_1")
+                    : new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            });
+
+        var loggerProvider = new CollectingLoggerProvider();
+        var client = BuildClient(env, mockHttp, new DeliveryOptions
+        {
+            EnvironmentId = env,
+            EnableResilience = false
+        }, loggerProvider);
+
+        await foreach (var _ in client.GetAssetUsedIn("asset_codename").EnumerateItemsAsync())
+        {
+            // Intentionally empty - verify log side effect.
+        }
+
+        Assert.Contains(
+            loggerProvider.Entries,
+            entry => entry.EventId == Kontent.Ai.Delivery.Logging.LogEventIds.PaginationStoppedEarly &&
+                     entry.Message.Contains("AssetUsedIn", StringComparison.Ordinal));
+    }
+
+    private static IDeliveryClient BuildClient(
+        string env,
+        MockHttpMessageHandler mockHttp,
+        DeliveryOptions? options = null,
+        ILoggerProvider? loggerProvider = null)
     {
         var services = new ServiceCollection();
+        if (loggerProvider is not null)
+        {
+            services.AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Debug);
+                builder.AddProvider(loggerProvider);
+            });
+        }
+
+        options ??= new DeliveryOptions { EnvironmentId = env };
         services.AddDeliveryClient(
-            new DeliveryOptions { EnvironmentId = env },
+            options,
             configureHttpClient: b => b.ConfigurePrimaryHttpMessageHandler(() => mockHttp));
 
         return services.BuildServiceProvider().GetRequiredService<IDeliveryClient>();
@@ -82,5 +223,43 @@ public sealed class UsedInQueriesTests
             response.Headers.Add("X-Continuation", continuationToken);
 
         return response;
+    }
+
+    private sealed record LogEntry(int EventId, string Message);
+
+    private sealed class CollectingLoggerProvider : ILoggerProvider
+    {
+        private readonly List<LogEntry> _entries = [];
+
+        public IReadOnlyList<LogEntry> Entries => _entries;
+
+        public ILogger CreateLogger(string categoryName) => new CollectingLogger(_entries);
+
+        public void Dispose()
+        {
+        }
+    }
+
+    private sealed class CollectingLogger(List<LogEntry> entries) : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state) where TState : notnull => NoopDisposable.Instance;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception? exception,
+            Func<TState, Exception?, string> formatter) => entries.Add(new LogEntry(eventId.Id, formatter(state, exception)));
+    }
+
+    private sealed class NoopDisposable : IDisposable
+    {
+        public static NoopDisposable Instance { get; } = new();
+
+        public void Dispose()
+        {
+        }
     }
 }