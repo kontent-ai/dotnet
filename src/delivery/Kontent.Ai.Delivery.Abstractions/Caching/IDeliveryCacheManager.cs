kontent-ai/delivery-sdk-net

refactor caching, fix potential circular deps for redis caching

Kontent.Ai.Delivery.Abstractions/Caching/IDeliveryCacheManager.cs
@@ -27,6 +27,18 @@ namespace Kontent.Ai.Delivery.Abstractions;
 public interface IDeliveryCacheManager
 {
     /// <summary>
+    /// Gets the storage mode used by this cache manager.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// <see cref="CacheStorageMode.Object"/> (default) stores fully hydrated C# objects,
+    /// suitable for in-memory caches. <see cref="CacheStorageMode.RawPayload"/> stores raw JSON
+    /// strings, suitable for distributed caches that require serialization.
+    /// </para>
+    /// </remarks>
+    CacheStorageMode StorageMode => CacheStorageMode.Object;
+
+    /// <summary>
     /// Attempts to retrieve a cached value by its key.
     /// </summary>
     /// <typeparam name="T">The type of the cached value.</typeparam>

Kontent.Ai.Delivery/Caching/DistributedCacheManager.cs
@@ -19,9 +19,10 @@ namespace Kontent.Ai.Delivery.Caching;
 /// </list>
 /// </para>
 /// <para>
-/// Serialization uses System.Text.Json with ReferenceHandler.Preserve to handle circular references.
-/// The JsonSerializerOptions are configured to handle deep object graphs and null values gracefully.
-/// Serialization failures throw InvalidOperationException with helpful error messages.
+/// The SDK caches raw JSON strings (via <see cref="CachedItemResponseRaw"/> and
+/// <see cref="CachedItemListingResponseRaw"/>) rather than hydrated C# objects.
+/// This approach avoids serialization issues with complex object graphs (circular references,
+/// custom converters, etc.) and simplifies the serialization configuration.
 /// </para>
 /// <para>
 /// The reverse index entries share the same expiration as their associated cache entries,
@@ -67,6 +68,9 @@ internal sealed class DistributedCacheManager : IDeliveryCacheManager
     private const string CacheKeyPrefix = "cache:";
     private const string DependencyKeyPrefix = "dep:";
 
+    /// <inheritdoc />
+    public CacheStorageMode StorageMode => CacheStorageMode.RawPayload;
+
     private string KeyPrefixSegment => string.IsNullOrEmpty(_keyPrefix) ? "" : $"{_keyPrefix}:";
 
     /// <summary>
@@ -105,10 +109,8 @@ internal sealed class DistributedCacheManager : IDeliveryCacheManager
     /// Default expiration time for cache entries. If null, defaults to 1 hour.
     /// </param>
     /// <param name="jsonSerializerOptions">
-    /// Custom JSON serialization options. If null, default options with ReferenceHandler.Preserve are used.
-    /// When caching SDK response types (ContentItem, DeliveryResult), provide the options from
-    /// <see cref="Configuration.RefitSettingsProvider.CreateDefaultJsonSerializerOptions"/> to ensure
-    /// proper serialization of content items.
+    /// Custom JSON serialization options. If null, simple default options are used.
+    /// The SDK caches raw JSON strings, so complex reference handling is not needed.
     /// </param>
     /// <param name="logger">Optional logger for cache operations.</param>
     /// <exception cref="ArgumentNullException">
@@ -126,16 +128,13 @@ internal sealed class DistributedCacheManager : IDeliveryCacheManager
         _defaultExpiration = defaultExpiration ?? TimeSpan.FromHours(1);
         _logger = logger;
 
-        _jsonOptions = jsonSerializerOptions is not null
-            ? jsonSerializerOptions
-            : new JsonSerializerOptions
-            {
-                ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve,
-                WriteIndented = false,
-                PropertyNameCaseInsensitive = true,
-                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                MaxDepth = 124
-            };
+        // Simple serialization options - SDK caches raw JSON strings, not complex object graphs
+        _jsonOptions = jsonSerializerOptions ?? new JsonSerializerOptions
+        {
+            WriteIndented = false,
+            PropertyNameCaseInsensitive = true,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        };
     }
 
     /// <inheritdoc />

kontent-ai/dotnet

src/delivery/Kontent.Ai.Delivery.Abstractions/Caching/IDeliveryCacheManager.cs
@@ -27,6 +27,18 @@ namespace Kontent.Ai.Delivery.Abstractions;
 public interface IDeliveryCacheManager
 {
     /// <summary>
+    /// Gets the storage mode used by this cache manager.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// <see cref="CacheStorageMode.HydratedObject"/> (default) stores fully hydrated C# objects,
+    /// suitable for in-memory caches. <see cref="CacheStorageMode.RawJson"/> stores raw JSON
+    /// strings, suitable for distributed caches that require serialization.
+    /// </para>
+    /// </remarks>
+    CacheStorageMode StorageMode => CacheStorageMode.HydratedObject;
+
+    /// <summary>
     /// Attempts to retrieve a cached value by its key.
     /// </summary>
     /// <typeparam name="T">The type of the cached value.</typeparam>

src/delivery/Kontent.Ai.Delivery/Caching/DistributedCacheManager.cs
@@ -19,9 +19,10 @@ namespace Kontent.Ai.Delivery.Caching;
 /// </list>
 /// </para>
 /// <para>
-/// Serialization uses System.Text.Json with ReferenceHandler.Preserve to handle circular references.
-/// The JsonSerializerOptions are configured to handle deep object graphs and null values gracefully.
-/// Serialization failures throw InvalidOperationException with helpful error messages.
+/// The SDK caches raw JSON strings (via <see cref="CachedItemResponseRaw"/> and
+/// <see cref="CachedItemListingResponseRaw"/>) rather than hydrated C# objects.
+/// This approach avoids serialization issues with complex object graphs (circular references,
+/// custom converters, etc.) and simplifies the serialization configuration.
 /// </para>
 /// <para>
 /// The reverse index entries share the same expiration as their associated cache entries,
@@ -67,6 +68,9 @@ internal sealed class DistributedCacheManager : IDeliveryCacheManager
     private const string CacheKeyPrefix = "cache:";
     private const string DependencyKeyPrefix = "dep:";
 
+    /// <inheritdoc />
+    public CacheStorageMode StorageMode => CacheStorageMode.RawJson;
+
     private string KeyPrefixSegment => string.IsNullOrEmpty(_keyPrefix) ? "" : $"{_keyPrefix}:";
 
     /// <summary>
@@ -105,10 +109,8 @@ internal sealed class DistributedCacheManager : IDeliveryCacheManager
     /// Default expiration time for cache entries. If null, defaults to 1 hour.
     /// </param>
     /// <param name="jsonSerializerOptions">
-    /// Custom JSON serialization options. If null, default options with ReferenceHandler.Preserve are used.
-    /// When caching SDK response types (ContentItem, DeliveryResult), provide the options from
-    /// <see cref="Configuration.RefitSettingsProvider.CreateDefaultJsonSerializerOptions"/> to ensure
-    /// proper serialization of content items.
+    /// Custom JSON serialization options. If null, simple default options are used.
+    /// The SDK caches raw JSON strings, so complex reference handling is not needed.
     /// </param>
     /// <param name="logger">Optional logger for cache operations.</param>
     /// <exception cref="ArgumentNullException">
@@ -126,16 +128,13 @@ internal sealed class DistributedCacheManager : IDeliveryCacheManager
         _defaultExpiration = defaultExpiration ?? TimeSpan.FromHours(1);
         _logger = logger;
 
-        _jsonOptions = jsonSerializerOptions is not null
-            ? jsonSerializerOptions
-            : new JsonSerializerOptions
-            {
-                ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve,
-                WriteIndented = false,
-                PropertyNameCaseInsensitive = true,
-                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                MaxDepth = 124
-            };
+        // Simple serialization options - SDK caches raw JSON strings, not complex object graphs
+        _jsonOptions = jsonSerializerOptions ?? new JsonSerializerOptions
+        {
+            WriteIndented = false,
+            PropertyNameCaseInsensitive = true,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        };
     }
 
     /// <inheritdoc />