6833879f6cbb80677c76e93766b7e1962884c986
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2026-09-25 13:26:45 +0200

    encode resolver values and show scheduling alternatives as comments in code samples

--- src/management/Kontent.Ai.Management.Tests/CodeSamples/CmApiV2.cs
+++ src/management/Kontent.Ai.Management.Tests/CodeSamples/CmApiV2.cs
@@ -1961,15 +1961,15 @@ public class CmApiV2
         var identifier = LanguageVariantIdentifier.ByIds(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474"), Guid.Parse("d1f95fde-af02-b3b5-bd9e-f232311ccab8"));
         // var identifier = LanguageVariantIdentifier.ByCodenames("my_article", "es-ES");
 
-        // Immediate publish
-        (await client.PublishLanguageVariantAsync(identifier)).EnsureSuccess();
-
         // Scheduled publish
         (await client.SchedulePublishingOfLanguageVariantAsync(identifier, new ScheduleModel
         {
             ScheduledTo = new DateTimeOffset(2038, 1, 19, 4, 14, 8, TimeSpan.Zero),
             DisplayTimeZone = "Australia/Sydney"
         })).EnsureSuccess();
+
+        // To publish now, use PublishLanguageVariantAsync instead
+        // (await client.PublishLanguageVariantAsync(identifier)).EnsureSuccess();
         // EndDocSection
     }
 
@@ -1983,15 +1983,15 @@ public class CmApiV2
         var identifier = LanguageVariantIdentifier.ByIds(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474"), Guid.Parse("d1f95fde-af02-b3b5-bd9e-f232311ccab8"));
         // var identifier = LanguageVariantIdentifier.ByCodenames("my_article", "es-ES");
 
-        // Immediate unpublish
-        (await client.UnpublishLanguageVariantAsync(identifier)).EnsureSuccess();
-
         // Scheduled unpublish
         (await client.ScheduleUnpublishingOfLanguageVariantAsync(identifier, new ScheduleModel
         {
             ScheduledTo = new DateTimeOffset(2038, 1, 19, 4, 14, 8, TimeSpan.Zero),
             DisplayTimeZone = "Australia/Sydney"
         })).EnsureSuccess();
+
+        // To unpublish now, use UnpublishLanguageVariantAsync instead
+        // (await client.UnpublishLanguageVariantAsync(identifier)).EnsureSuccess();
         // EndDocSection
     }
 
